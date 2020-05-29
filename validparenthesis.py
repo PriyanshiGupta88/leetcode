774bb87f1123882fc776c4a9e8f607ea6a5e58f5
PriyanshiGupta88 <46122094+PriyanshiGupta88@users.noreply.github.com>
2020-05-29 07:50:23 -0700

    Create validparenthesis.py

---
+++ validparenthesis.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while "()" in s or "{}" in s or '[]' in s:
+            s = s.replace("()", "").replace('{}', "").replace('[]', "")
+        return s == ''
+        
